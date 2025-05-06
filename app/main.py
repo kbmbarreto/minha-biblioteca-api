56085a2b89fa4a6527933a1868d7aaa4da4f7fd0
kleber.barreto <kleber.barreto@msn.com>
2025-05-06 18:23:53 -0300

    Adicionado ajuste para conexão com o front-end

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # Permitir acesso do Angular
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
